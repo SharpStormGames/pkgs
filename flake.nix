1755a7cde61312ada158ac47462da4e3c599d397
SharpStormGames <112800647+SharpStormGames@users.noreply.github.com>
2024-07-06 16:33:26 +1000

    Flake: Modify Alpine Client pkg

--- flake.nix
+++ flake.nix
@@ -27,19 +27,25 @@
     stdenv.mkDerivation {
      name = "alpine-client-1.6.0";
      version = "1.6.0";
-     nativeBuildInputs = [ rpmextract ];
+     nativeBuildInputs = [ rpmextract autoPatchelfHook ];
+     autoPatchelfIgnoreMissingDeps = [ "libGL.so.1" "libX11.so.6" "libstdc++.so.6" "libgcc_s.so.1" ];
      src = pkgs.fetchurl {
       url = "https://api.alpineclientprod.com/uploads/alpine_client_1_6_0_94_x86_64_3256716d3d.rpm";
       sha256 = "sha256-+F7pAGkuHvYoNwVktKUjy5EUWUv5HTaDrlkQpU/oOT4=";
      };
-     dontPatch = true;
-     dontConfigure = true;
      dontBuild = true;
+     dontConfigure = true;
      unpackPhase = ''
-      rpmextract $src
+      mkdir $out/
+      mkdir $out/rpmsrc
+      cp $src $out/rpmsrc/src.rpm
+      cd $out/rpmsrc/
+      rpmextract src.rpm
      '';
      installPhase = ''
-      cp usr/ $out/ -r
+      runHook preInstall
+      install -m755 -D $out/rpmsrc/usr/bin/alpine-client $out/bin/alpine-client
+      runHook postinstall
      '';
     };
    };
