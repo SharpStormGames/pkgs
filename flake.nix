cf588cc7f14ed51012f54a55bb2391ca8a6d41a8
SharpStormGames <112800647+SharpStormGames@users.noreply.github.com>
2024-07-06 17:41:59 +1000

    Flake: Remove Alpine Client Package

--- flake.nix
+++ flake.nix
@@ -21,32 +21,5 @@
      src = ./grub-theme;
      installPhase = "mkdir -p $out; cp -r $src/* $out";
     };
-   # Alpine Client Tarball
-   packages.x86_64-linux.alpine-client = 
-    with import nixpkgs { system = "x86_64-linux"; };
-    stdenv.mkDerivation {
-     name = "alpine-client-1.6.0";
-     version = "1.6.0";
-     nativeBuildInputs = [ rpmextract autoPatchelfHook ];
-     buildInputs = [ libGL xorg.libX11 libgcc stdenv.cc.cc.lib ];
-     src = pkgs.fetchurl {
-      url = "https://api.alpineclientprod.com/uploads/alpine_client_1_6_0_94_x86_64_3256716d3d.rpm";
-      sha256 = "sha256-+F7pAGkuHvYoNwVktKUjy5EUWUv5HTaDrlkQpU/oOT4=";
-     };
-     dontBuild = true;
-     dontConfigure = true;
-     unpackPhase = ''
-      mkdir $out/
-      mkdir $out/rpmsrc
-      cp $src $out/rpmsrc/src.rpm
-      cd $out/rpmsrc/
-      rpmextract src.rpm
-     '';
-     installPhase = ''
-      runHook preInstall
-      install -m755 -D $out/rpmsrc/usr/bin/alpine-client $out/bin/alpine-client
-      runHook postinstall
-     '';
-    };
    };
 }
