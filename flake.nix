a29a7aced3ffc75cb484bc85a7ec793cb62ccddd
SharpStormGames <112800647+SharpStormGames@users.noreply.github.com>
2024-07-10 13:53:49 +1000

    Flake: Small Modifications

--- flake.nix
+++ flake.nix
@@ -1,69 +1,59 @@
 {
  description = "Flake to create packages for my dotfiles";
  inputs = { nixpkgs = { url = "github:nixos/nixpkgs?ref=nixos-unstable"; }; };
- outputs = { self, nixpkgs }: 
-  let
-   pkgs = import nixpkgs { system = "x86_64-linux"; };
-   # Alpine Client Launcher Bootstrapper SRC
-   pinnacle =
-    with import nixpkgs { system = "x86_64-linux"; };
-    stdenv.mkDerivation {
-     pname = "pinnacle";
-     version = "1.6.0";
-     dontUnpack = true;
-     dontBuild = true;
-     dontConfigure = true;
-     src = pkgs.fetchurl {
-      url = "https://github.com/alpine-client/pinnacle/releases/download/1.6.0/pinnacle-linux-amd64";
-      sha256 = "sha256-D7yWOHz792jeS8GglY1DsVwKOHGbNHT9jWujofFSEok=";
-     };
-     installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/pinnacle
-      install -m755 -D $out/pinnacle $out/bin/
-    '';
-    };
-  in {    
+ outputs = { self, nixpkgs }: let pkgs = import nixpkgs { system = "x86_64-linux"; }; in {    
   packages.x86_64-linux = {
-   # Firefox Theme
-   ff-theme = 
-    with import nixpkgs { system = "x86_64-linux"; };
-    stdenv.mkDerivation {
-     name = "ff-theme";
-     src = ./ff-theme;
-     installPhase = "mkdir -p $out; cp -r $src/* $out";
-    };
-   # GNU GRUB Theme
-   grub-theme = 
-    with import nixpkgs { system = "x86_64-linux"; };
-    stdenv.mkDerivation {
-     name = "grub-theme";
-     src = ./grub-theme;
-     installPhase = "mkdir -p $out; cp -r $src/* $out";
-    };
-   # Alpine Client Launcher
-    alpine-client = pkgs.buildFHSEnv {
+   ff-theme = pkgs.stdenv.mkDerivation {
+    name = "ff-theme";
+    src = ./ff-theme;
+    installPhase = "mkdir -p $out; cp -r $src/* $out";
+   };
+   grub-theme = pkgs.stdenv.mkDerivation {
+    name = "grub-theme";
+    src = ./grub-theme;
+    installPhase = "mkdir -p $out; cp -r $src/* $out";
+   };
+    alpine-client = 
+    let
+     pinnacle =
+     pkgs.stdenv.mkDerivation {
+      pname = "pinnacle";
+      version = "1.6.0";
+      dontUnpack = true;
+      dontBuild = true;
+      dontConfigure = true;
+      src = pkgs.fetchurl {
+       url = "https://github.com/alpine-client/pinnacle/releases/download/1.6.0/pinnacle-linux-amd64";
+       sha256 = "sha256-D7yWOHz792jeS8GglY1DsVwKOHGbNHT9jWujofFSEok=";
+      };
+      installPhase = ''
+       mkdir -p $out/bin
+       cp $src $out/pinnacle
+       install -m755 -D $out/pinnacle $out/bin/
+     '';
+     };
+    in 
+    pkgs.buildFHSEnv {
      name = "alpine-client";
-     targetPkgs = pkgs: [ 
+     targetPkgs = pkgs: with pkgs; [ 
       pinnacle
-      pkgs.alsa-lib
-      pkgs.libGL 
-      pkgs.xorg.libX11 
-      pkgs.xorg.libXcursor
-      pkgs.xorg.libXext 
-      pkgs.xorg.libXrender 
-      pkgs.xorg.libXtst
-      pkgs.xorg.libXi
-      pkgs.xorg.libXrandr
-      pkgs.xorg.libXxf86vm
-      pkgs.libgcc 
-      pkgs.libz 
-      pkgs.stdenv.cc.cc.lib
-      pkgs.fontconfig
+      alsa-lib
+      libGL 
+      xorg.libX11 
+      xorg.libXcursor
+      xorg.libXext 
+      xorg.libXrender 
+      xorg.libXtst
+      xorg.libXi
+      xorg.libXrandr
+      xorg.libXxf86vm
+      libgcc 
+      libz 
+      stdenv.cc.cc.lib
+      fontconfig
      ];
      runScript = "pinnacle";
     };
-   # where-is-my-sddm-theme override
     sddm-theme = pkgs.where-is-my-sddm-theme.override {
      variants = ["qt5"];
      themeConfig.General = { 
