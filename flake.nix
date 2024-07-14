0db2241bf065e0c71f1c4c1cc1a85fc7bb23d620
SharpStormGames <112800647+SharpStormGames@users.noreply.github.com>
2024-07-14 15:55:19 +1000

    Add alpine icon pkg

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     src = ./grub-theme;
     installPhase = "mkdir -p $out; cp -r $src/* $out";
    };
-    alpine-client = 
+   alpine-client = 
     let
      pinnacle =
      pkgs.stdenv.mkDerivation {
@@ -54,13 +54,25 @@
      ];
      runScript = "pinnacle";
     };
-    sddm-theme = pkgs.where-is-my-sddm-theme.override {
-     variants = ["qt5"];
-     themeConfig.General = { 
-      passwordFontSize = 24; 
-      passwordCursorColor = "white"; 
-     };
+   alpine-icon = pkgs.stdenv.mkDerivation {
+    name = "alpine-icon";
+    src = pkgs.fetchurl {
+     url = "https://github.com/alpine-client/pinnacle/blob/084fde184a816b2241c36bd4f37a9fced3f21dbf/assets/icon.png?raw=true";
+     sha256 = "sha256-heIzTgeTQy3UgA/j/L6jd72Rn+k4wINPRWob1xEGSwA=";
     };
+    dontUnpack = true;
+    dontPatch = true;
+    dontBuild = true;
+    installPhase = "mkdir -p $out; cp $src $out/icon.png";
+   };
+   
+   sddm-theme = pkgs.where-is-my-sddm-theme.override {
+    variants = ["qt5"];
+    themeConfig.General = { 
+     passwordFontSize = 24; 
+     passwordCursorColor = "white"; 
+    };
+   };
   };
  };
 }
