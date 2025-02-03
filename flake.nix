a5fadafbfcc06032cd9bb6fc7092ac6c171b1d8d
SharpStormGames <112800647+SharpStormGames@users.noreply.github.com>
2025-02-04 10:15:33 +1100

    Add Firefox Profile Switcher Connector

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
-  outputs = { self, nixpkgs }: let pkgs = import nixpkgs { system = "x86_64-linux"; }; in {    
+  outputs = { self, nixpkgs }: let pkgs = import nixpkgs { system = "x86_64-linux"; }; in {
     packages.x86_64-linux = {
       alpine-client = import ./alpine-client/package.nix { inherit self pkgs; };
       alpine-icon = import ./alpine-client/icon.nix { inherit self pkgs; };
       element-desktop-nightly = import ./element-nightly/package.nix { inherit self pkgs; };
+      profile-connector = import ./profile-connector/package.nix { inherit self pkgs; };
       # TODO: Move to themes repo
       grub-theme = pkgs.stdenv.mkDerivation {
         name = "grub-theme";
---
+++ profile-connector/package.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "firefox-profile-switcher-connector";
+  version = "0.1.1";
+  src = pkgs.fetchFromGitHub {
+    owner = "null-dev";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-mnPpIJ+EQAjfjhrSSNTrvCqGbW0VMy8GHbLj39rR8r4=";
+  };
+  nativeBuildInputs = [ pkgs.cmake ];
+  cargoHash = "sha256-EQIBeZwF9peiwpgZNfMmjvLv8NyhvVGUjVXgkf12Wig=";
+  postInstall = ''
+  mkdir -p $out/lib/mozilla/native-messaging-hosts
+  sed -i s#/usr/bin/ff-pswitch-connector#$out/bin/firefox_profile_switcher_connector# manifest/manifest-linux.json
+  cp manifest/manifest-linux.json $out/lib/mozilla/native-messaging-hosts/ax.nd.profile_switcher_ff.json
+  '';
+}
