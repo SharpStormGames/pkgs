6ac6006cc50079e72ca9a7d9aa7890b1fbeaa410
SharpStormGames <112800647+SharpStormGames@users.noreply.github.com>
2025-02-11 20:46:57 +1100

    Profile-Connector: Move off deprecated opts

--- profile-connector/package.nix
+++ profile-connector/package.nix
@@ -10,7 +10,8 @@ pkgs.rustPlatform.buildRustPackage rec {
     sha256 = "sha256-mnPpIJ+EQAjfjhrSSNTrvCqGbW0VMy8GHbLj39rR8r4=";
   };
   nativeBuildInputs = [ pkgs.cmake ];
-  cargoHash = "sha256-EQIBeZwF9peiwpgZNfMmjvLv8NyhvVGUjVXgkf12Wig=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-Bp3UxuFQFvh/htV4sVcgOKe0w1+Ed6Iz6c9l9PWrW5E=";
   postInstall = ''
   mkdir -p $out/lib/mozilla/native-messaging-hosts
   sed -i s#/usr/bin/ff-pswitch-connector#$out/bin/firefox_profile_switcher_connector# manifest/manifest-linux.json
